f1c241118f629d494a5498e3501bb30172c0fbb4
aschwartz01 <adschwartz01@gmail.com>
2024-03-13 15:37:06 -0400

    Committing the encrypter and decrypter

---
+++ src/ccrypt/__main__.py
@@ -0,0 +1,11 @@
+import caesar_crypt as caesar
+
+if __name__ == "__main__":
+    original_text = "My name is Adam Schwartz and I was born in 2001!"
+    shift_amount = 26
+
+    encrypted = caesar.caesar_encrypt(original_text, shift_amount)
+    print(f"Encrypted: {encrypted}")
+
+    decrypted = caesar.caesar_decrypt(encrypted, shift_amount)
+    print(f"Decrypted: {decrypted}")
