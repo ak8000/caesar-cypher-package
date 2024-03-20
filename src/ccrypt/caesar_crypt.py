fca5db76cd2f685aaabe915b327832702d320012
ak8000 <ak8000@nyu.edu>
2024-03-20 11:39:32 -0400

    encrypt + main

--- src/ccrypt/caesar_crypt.py
+++ src/ccrypt/caesar_crypt.py
@@ -1,44 +1,29 @@
 def caesar_encrypt(text, shift):
+    """
+    Encrypts a given text using the Caesar cipher, extended to include numbers.
+
+    :param text: The input string to encrypt.
+    :param shift: The number of positions to shift each character.
+    :return: The encrypted string.
+    """
     encrypted_text = ""
+    
     for char in text:
-        if char.isalpha():
-            shift_amount = shift % 26
-            if(shift % 26 == 0):
-                if(shift > 0):
-                    shift_amount = (shift + 1) % 26
-                else:
-                    shift_amount = (shift - 1) % 26
-
-            char_code = ord(char) + shift_amount
-
-            if char.isupper():
-                if char_code > ord('Z'):
-                    char_code -= 26
-                elif char_code < ord('A'):
-                    char_code += 26
-            elif char.islower():
-                if char_code > ord('z'):
-                    char_code -= 26
-                elif char_code < ord('a'):
-                    char_code += 26
-
-            encrypted_text += chr(char_code)
+        if char.isupper():
+            encrypted_char = chr((ord(char) + shift - 65) % 26 + 65)
+        elif char.islower():
+            encrypted_char = chr((ord(char) + shift - 97) % 26 + 97)
         elif char.isdigit():
-            shift_amount = shift % 10
-            if(shift % 10 == 0):
-                if(shift > 0):
-                    shift_amount = (shift + 1) % 10
-                else:
-                    shift_amount = (shift - 1) % 10
-
-            new_digit = (int(char) + shift_amount) % 10
-            encrypted_text += str(new_digit)
+            encrypted_char = chr((ord(char) + shift - 48) % 10 + 48)
         else:
-            encrypted_text += char
+            encrypted_char = char
+        encrypted_text += encrypted_char
+
     return encrypted_text
 
 def caesar_decrypt(text, shift):
     return caesar_encrypt(text, -shift)
+
 
 def verify_crypt(pre_encrpted_text, post_decrypted_text):
     if(pre_encrpted_text == post_decrypted_text):
